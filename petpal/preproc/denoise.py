6edeb7e9cb1c72188eadf6a75a4f622ec628a3af
kenanoestreich <kenan.oestreich@gmail.com>
2024-10-03 09:56:59 -0500

    feat: add a few function signatures

--- petpal/preproc/denoise.py
+++ petpal/preproc/denoise.py
@@ -2,13 +2,14 @@
 import logging
 import os
 
+import numpy as np
 from nibabel.filebasedimages import FileBasedImage
-from numpy import ndarray
+from sklearn.cluster import k_means
 
 from ..utils.image_io import ImageIO
-import numpy as np
 
 logger = logging.getLogger(__name__)
+
 
 def denoise_image(pet_image: np.ndarray,
                   t1_image: np.ndarray,
@@ -17,12 +18,33 @@ def denoise_image(pet_image: np.ndarray,
     pass
 
 
-def prepare_inputs(path_to_pet: str,
-                   path_to_mri: str,
-                   path_to_freesurfer_segmentation: str) -> [np.ndarray]:
-    """Read images from files into ndarrays, and ensure all images have the same dimensions as PET"""
+def apply_k_means_clustering(data: np.ndarray,
+                             num_clusters: int or list[int],
+                             **kwargs) -> [np.ndarray, np.ndarray]:
+    """Separate data into num_clusters clusters using Lloyd's algorithm implemented in sklearn, with recursive option"""
+    pass
 
-    logger.debug("Logger Attached")
+
+def rearrange_voxels_to_wheel_space():
+    """Use voxelwise distances from cluster feature averages to arrange voxel indices onto 2D 'wheel' space."""
+    pass
+
+
+def apply_smoothing_in_sinogram_space():
+    """Smooth clustered feature image on its sinogram space using radon transform and predefined smoothing kernel."""
+    pass
+
+
+def temporal_pca():
+    """Run principal component analysis on spatially-flattened PET dataset and return PC1, 2, and 3 scores for each index."""
+    pass
+
+
+def _prepare_inputs(path_to_pet: str,
+                    path_to_mri: str,
+                    path_to_freesurfer_segmentation: str) -> [np.ndarray]:
+    """Read images from files into ndarrays, and ensure all images have the same dimensions as PET."""
+
     images_loaded = []
     images_failed_to_load = []
     errors = []
@@ -47,8 +69,8 @@ def prepare_inputs(path_to_pet: str,
     pet_data_3d_shape = pet_data.shape[:-1]
 
     if mri_data.shape != pet_data_3d_shape or segmentation_data.shape != pet_data_3d_shape:
-        raise Exception(f'MRI and/or Segmentation has different dimensions from PET image:\n'
-                        f'PET Shape: {pet_data.shape}\n'
+        raise Exception(f'MRI and/or Segmentation has different dimensions from 3D PET image:\n'
+                        f'PET Frame Shape: {pet_data_3d_shape}\n'
                         f'Segmentation Shape: {segmentation_data.shape}\n'
                         f'MRI Shape: {mri_data.shape}.\n'
                         f'Ensure that all non-PET data is registered to PET space')
