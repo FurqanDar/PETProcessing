db346df0439f86bb0e3137a1c0ec06e365379118
kenanoestreich <kenan.oestreich@gmail.com>
2024-10-17 16:07:51 -0500

    feat: add a few function outlines

--- petpal/preproc/denoise.py
+++ petpal/preproc/denoise.py
@@ -79,95 +79,112 @@ class Denoiser:
         return pet_data, mri_data, segmentation_data
 
 
-def apply_3_tier_k_means_clustering(flattened_feature_data: np.ndarray,
-                                    num_clusters: list[int],
-                                    **kwargs) -> (np.ndarray, np.ndarray):
-    """Separate data into num_clusters clusters using Lloyd's algorithm implemented in sklearn.
+    def apply_3_tier_k_means_clustering(self,
+                                        flattened_feature_data: np.ndarray,
+                                        num_clusters: list[int],
+                                        **kwargs) -> (np.ndarray, np.ndarray):
+        """Separate data into num_clusters clusters using Lloyd's algorithm implemented in sklearn.
 
-    This function performs k-means clustering "recursively" on feature data from a (PET) image. The input data should be 2D,
-    where one dimension corresponds to all the voxels in a single 3D PET Frame, and the other dimension corresponds to
-    the feature values for those voxels. Example features include Temporal PCA components from PET, T1 or T2 MRI
-    intensity, freesurfer segmentation. Note that MRI and segmentation data must be registered to native PET space.
-    This input data is clustered with k-means 3 successive times, with each cluster from the first tier being passed
-    into the second tier, and so on for the third tier. The final number of clusters is considered to be the product of
-    num_cluster's elements, since the final tier's cluster outputs are returned.
+        This function performs k-means clustering "recursively" on feature data from a (PET) image. The input data should be 2D,
+        where one dimension corresponds to all the voxels in a single 3D PET Frame, and the other dimension corresponds to
+        the feature values for those voxels. Example features include Temporal PCA components from PET, T1 or T2 MRI
+        intensity, freesurfer segmentation. Note that MRI and segmentation data must be registered to native PET space.
+        This input data is clustered with k-means 3 successive times, with each cluster from the first tier being passed
+        into the second tier, and so on for the third tier. The final number of clusters is considered to be the product of
+        num_cluster's elements, since the final tier's cluster outputs are returned.
 
-    Args:
-        flattened_feature_data (np.ndarray): Feature data from PET (and other sources) to cluster. Must be 2D, where one
-            dimension corresponds to all the voxels in a single 3D PET Frame, and the other dimension corresponds to the
-            feature values for those voxels.
-        num_clusters (list[int]): Number of clusters to use in each tier of k_means clustering. num_clusters must have
-            a length of 3, where the value at the first index is the number of clusters in the first tier, and so on.
-        **kwargs: Additional keyword arguments passed to the `sklearn.cluster.k_means` method.
+        Args:
+            flattened_feature_data (np.ndarray): Feature data from PET (and other sources) to cluster. Must be 2D, where one
+                dimension corresponds to all the voxels in a single 3D PET Frame, and the other dimension corresponds to the
+                feature values for those voxels.
+            num_clusters (list[int]): Number of clusters to use in each tier of k_means clustering. num_clusters must have
+                a length of 3, where the value at the first index is the number of clusters in the first tier, and so on.
+            **kwargs: Additional keyword arguments passed to the `sklearn.cluster.k_means` method.
 
-    Returns:
-        Tuple[np.ndarray, np.ndarray]: First array contains the feature centroids for each cluster, and the second
-        contains the cluster labels for each "voxel" in the input data. See sklearn.cluster.k_means documentation for
-        more details.
+        Returns:
+            Tuple[np.ndarray, np.ndarray]: First array contains the feature centroids for each cluster, and the second
+            contains the cluster labels for each "voxel" in the input data. See sklearn.cluster.k_means documentation for
+            more details.
 
-    """
+        """
 
-    # Verify format of inputs
-    if len(num_clusters) != 3:
-        raise IndexError(
-            'num_clusters must be a list of length 3, where num_clusters[0] is the number of clusters at the top-level,'
-            ' num_clusters[1] is the number of clusters to separate each of the top-level clusters into, and so on.')
+        # Verify format of inputs
+        if len(num_clusters) != 3:
+            raise IndexError(
+                'num_clusters must be a list of length 3, where num_clusters[0] is the number of clusters at the top-level,'
+                ' num_clusters[1] is the number of clusters to separate each of the top-level clusters into, and so on.')
 
-    if flattened_feature_data.ndim != 2:
-        raise IndexError(
-            'flattened_feature_data input MUST be a 2-D numpy array, where the first dimension corresponds to the samples, '
-            'and the second dimension corresponds to the features')
+        if flattened_feature_data.ndim != 2:
+            raise IndexError(
+                'flattened_feature_data input MUST be a 2-D numpy array, where the first dimension corresponds to the samples, '
+                'and the second dimension corresponds to the features')
 
-    # Dimensions will be (# of clusters, # of features)
-    centroids = np.zeros(shape=(np.prod(num_clusters), flattened_feature_data.shape[1]))
-    _, cluster_ids, _ = k_means(X=flattened_feature_data,
-                                n_clusters=num_clusters[0],
-                                **kwargs)
+        # Dimensions will be (# of clusters, # of features)
+        centroids = np.zeros(shape=(np.prod(num_clusters), flattened_feature_data.shape[1]))
+        _, cluster_ids, _ = k_means(X=flattened_feature_data,
+                                    n_clusters=num_clusters[0],
+                                    **kwargs)
 
-    cluster_ids_2 = np.zeros(shape=cluster_ids.shape)
-    for cluster in range(num_clusters[0]):
-        logger.debug(f'Top-Level Cluster ID: {cluster}')
-        cluster_data = flattened_feature_data[cluster_ids == cluster, :]
-        logger.debug(f'{cluster_data}\n{cluster_data.shape}')
-        _, cluster_ids_temp, _ = k_means(X=cluster_data,
-                                         n_clusters=num_clusters[1],
-                                         **kwargs)
-        logger.debug(f'cluster_ids_temp\n{cluster_ids_temp}\n{cluster_ids_temp.shape}')
-        cluster_ids_2[cluster_ids == cluster] = cluster_ids[cluster_ids == cluster] * num_clusters[1] + cluster_ids_temp
+        cluster_ids_2 = np.zeros(shape=cluster_ids.shape)
+        for cluster in range(num_clusters[0]):
+            logger.debug(f'Top-Level Cluster ID: {cluster}')
+            cluster_data = flattened_feature_data[cluster_ids == cluster, :]
+            logger.debug(f'{cluster_data}\n{cluster_data.shape}')
+            _, cluster_ids_temp, _ = k_means(X=cluster_data,
+                                             n_clusters=num_clusters[1],
+                                             **kwargs)
+            logger.debug(f'cluster_ids_temp\n{cluster_ids_temp}\n{cluster_ids_temp.shape}')
+            cluster_ids_2[cluster_ids == cluster] = cluster_ids[cluster_ids == cluster] * num_clusters[1] + cluster_ids_temp
 
-    cluster_ids_3 = np.zeros(shape=cluster_ids.shape)
-    for cluster in range(num_clusters[0] * num_clusters[1]):
-        logger.debug(f'Mid-Level Cluster ID: {cluster}')
-        cluster_data = flattened_feature_data[cluster_ids_2 == cluster, :]
-        centroids_temp, cluster_ids_temp, _ = k_means(X=cluster_data,
-                                                      n_clusters=num_clusters[2],
-                                                      **kwargs)
-        cluster_ids_3[cluster_ids_2 == cluster] = cluster_ids_temp + num_clusters[2] * cluster
-        logger.debug(f'Centroids for cluster {cluster}\n{centroids_temp}\n{centroids_temp.shape}')
-        for sub_cluster in range(num_clusters[2]):
-            centroids[cluster * num_clusters[2] + sub_cluster, :] = centroids_temp[sub_cluster]
+        cluster_ids_3 = np.zeros(shape=cluster_ids.shape)
+        for cluster in range(num_clusters[0] * num_clusters[1]):
+            logger.debug(f'Mid-Level Cluster ID: {cluster}')
+            cluster_data = flattened_feature_data[cluster_ids_2 == cluster, :]
+            centroids_temp, cluster_ids_temp, _ = k_means(X=cluster_data,
+                                                          n_clusters=num_clusters[2],
+                                                          **kwargs)
+            cluster_ids_3[cluster_ids_2 == cluster] = cluster_ids_temp + num_clusters[2] * cluster
+            logger.debug(f'Centroids for cluster {cluster}\n{centroids_temp}\n{centroids_temp.shape}')
+            for sub_cluster in range(num_clusters[2]):
+                centroids[cluster * num_clusters[2] + sub_cluster, :] = centroids_temp[sub_cluster]
 
-    cluster_ids = cluster_ids + cluster_ids_3
+        cluster_ids = cluster_ids + cluster_ids_3
 
-    return centroids, cluster_ids
+        return centroids, cluster_ids
 
 
-def head_mask(pet_data: np.ndarray,
-              thresh: float = 500.0) -> np.ndarray:
-    """Function to extract 3D head mask PET data using basic morphological methods"""
+    def extract_cluster_distances(self):
+        """Calculate distances from centroids in feature space and 'ring' space for each voxel assigned to a cluster"""
+        pass
 
-    mean_slice = np.mean(pet_data, axis=3)
-    thresholded_data = threshold_binary(input_image_numpy=mean_slice, lower_bound=thresh)
-    kernel = np.ones(shape=(3, 3, 3))
-    neighbor_count = convolve(thresholded_data, kernel, mode='constant')
-    thresholded_data[neighbor_count < 14] = 0
-    mask_image = binary_fill_holes(thresholded_data)
 
-    return mask_image
+    def add_nonbrain_features_to_segmentation(self,
+                                              segmentation_data: np.ndarray,
+                                              spatially_flattened_pet_data: np.ndarray,
+                                              head_mask_data: np.ndarray) -> np.ndarray:
+        """Cluster non-brain and add labels to existing segmentation"""
+        pass
+
+
+    def map_to_wheel_space(self):
+        """Use voxelwise distances from cluster feature centroids to arrange voxels onto 2D 'ring map'."""
+        pass
+
+
+    def apply_smoothing_in_sinogram_space(self,
+                                          image_data: np.ndarray,
+                                          kernel: np.ndarray,
+                                          **kwargs) -> np.ndarray:
+        """Transform image to sinogram space, apply smoothing, and transform back to original domain"""
+        pass
+
+    def weighted_sum_smoothed_image_iterations(self):
+        """Weight smoothed images (one from each iteration) by cluster 'belongingness' with respect to MRI. """
+        pass
 
 
 def flatten_pet_spatially(pet_data: np.ndarray) -> np.ndarray:
-    """Flatten spatial dimensions (using C index order) of 4D PET and return 2D version"""
+    """Flatten spatial dimensions (using C index order) of 4D PET and return 2D array (numVoxels x numFrames)"""
 
     num_voxels = np.prod(pet_data.shape[:-1])
     flattened_pet_data = pet_data.reshape(num_voxels, -1)
@@ -175,34 +192,15 @@ def flatten_pet_spatially(pet_data: np.ndarray) -> np.ndarray:
     return flattened_pet_data
 
 
-def temporal_pca(flattened_pet_data: np.ndarray,
-                 flattened_head_mask: np.ndarray) -> np.ndarray:
-    """Run principal component analysis on spatially-flattened PET and return PC1, 2, 3 scores per index"""
-    # head_data = flattened_pet_data[flattened_head_mask, :]
-    # pca = PCA(n_components=2).fit(X=head_data)
+def head_mask(pet_data: np.ndarray,
+              thresh: float = 500.0) -> np.ndarray:
+    """Function to extract 3D head mask PET data using basic morphological methods"""
 
-    pass
+    mean_slice = np.mean(pet_data, axis=3) # TODO: Use weighted series sum instead; more reliable
+    thresholded_data = threshold_binary(input_image_numpy=mean_slice, lower_bound=thresh)
+    kernel = np.ones(shape=(3, 3, 3))
+    neighbor_count = convolve(thresholded_data, kernel, mode='constant')
+    thresholded_data[neighbor_count < 14] = 0
+    mask_image = binary_fill_holes(thresholded_data)
 
-
-def add_nonbrain_features_to_segmentation(segmentation_data: np.ndarray,
-                                          spatially_flattened_pet_data: np.ndarray,
-                                          head_mask_data: np.ndarray) -> np.ndarray:
-    """Cluster non-brain and add labels to existing segmentation"""
-    pass
-
-
-def denoise_image_data(pet_data: np.ndarray,
-                       t1_mri_data: np.ndarray,
-                       segmentation_data: np.ndarray) -> np.ndarray:
-    """Use Hamed Yousefi's method to denoise a PET image."""
-    pass
-
-
-def rearrange_voxels_to_wheel_space():
-    """Use voxelwise distances from cluster feature averages to arrange voxel indices onto 2D 'wheel' space."""
-    pass
-
-
-def apply_smoothing_in_sinogram_space():
-    """Smooth clustered feature image on its sinogram space using radon transform and predefined smoothing kernel."""
-    pass
+    return mask_image
